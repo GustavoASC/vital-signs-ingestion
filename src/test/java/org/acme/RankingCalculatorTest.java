9871bdaf6934f03ddf65d6c196168f6df75bdfc9
Gustavo Cassel <guscassel@gmail.com>
2022-09-08 08:43:58 -0300

    Update ranking calculator test to use Mockito

--- src/test/java/org/acme/RankingCalculatorTest.java
+++ src/test/java/org/acme/RankingCalculatorTest.java
@@ -1,23 +1,36 @@
 package org.acme;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class RankingCalculatorTest {
 
-    private RankingCalculator calculator;
+    @Mock
+    private ServicePriorityLocator servicePriorityLocator;
 
-    @BeforeEach
-    public void setUp() {
-        calculator = new RankingCalculator(service -> 6);
-    }
+    @InjectMocks
+    private RankingCalculator calculator;
 
     @Test
     public void shouldCalculateRankingForUserAndService() {
+        when(servicePriorityLocator.locate("foo"))
+                .thenReturn(6);
+
         assertThat(calculator.calculate(1, "foo"))
-            .isEqualTo(7);
+                .isEqualTo(7);
+
+        verify(servicePriorityLocator, times(1))
+                .locate(any());
     }
-    
+
 }

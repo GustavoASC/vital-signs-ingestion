43bc3323321adb38102ea7d35a9a62af45a38d23
Gustavo Cassel <guscassel@gmail.com>
2022-09-15 08:48:56 -0300

    Implement endpoint to receive percentage of used CPU

--- src/test/java/org/acme/ResourcesResourceIT.java
+++ src/test/java/org/acme/ResourcesResourceIT.java
@@ -1,22 +1,47 @@
 package org.acme;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.CoreMatchers.is;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.util.Objects;
+
+import javax.inject.Inject;
 
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 
 @QuarkusTest
 public class ResourcesResourceIT {
-    
+
+    @Inject
+    ResourcesLocator resourcesLocator;
+
     @Test
-    public void testHelloEndpoint() {
+    public void testHelloEndpoint() throws IOException {
         given()
-          .when().post("/resources")
-          .then()
-             .statusCode(200)
-             .body(is(""));
+            .contentType(ContentType.JSON)
+            .body(jsonFromResource("used-cpu-percentage.json"))
+        .when()
+            .post("/resources")
+        .then()
+            .statusCode(200)
+            .body(is(""));
+
+        assertThat(resourcesLocator.getUsedCpuPercentage())
+                .isEqualTo(37);
+    }
+
+    public String jsonFromResource(String resourcePath) throws IOException {
+        String fullResourcePath = Path.of("/resources", resourcePath).toString();
+        InputStream resourceStream = Objects.requireNonNull(this.getClass().getResourceAsStream(fullResourcePath));
+        return new String(resourceStream.readAllBytes(), Charset.defaultCharset());
     }
 
 }

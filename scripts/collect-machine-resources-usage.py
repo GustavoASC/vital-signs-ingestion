17efd5553991abc0add7ee91b5141e8d84fbe68f
Gustavo Cassel <guscassel@gmail.com>
2022-09-15 21:12:05 -0300

    Update Python script to send information about used cpu

--- scripts/collect-machine-resources-usage.py
+++ scripts/collect-machine-resources-usage.py
@@ -1,4 +1,19 @@
-import psutil
+import psutil, urllib.request, socket, json, traceback
+
+URL = "http://localhost:8080/resources"
+HOSTNAME = socket.gethostname()
 
 while True:
-    print(psutil.cpu_percent(interval=1))
+
+    try:
+        cpu_percent = psutil.cpu_percent(interval=1)
+        print(f"Current cpu: {cpu_percent}")
+
+        headers = {"content-type": "application/json"}
+        data = json.dumps({"hostname": HOSTNAME, "cpu": cpu_percent}).encode("utf-8")
+        req = urllib.request.Request(URL, data=data, headers=headers)
+        with urllib.request.urlopen(req) as response:
+            print(f"Response: {str(response.read())}")
+
+    except Exception as e:
+        print(f"Unexpected problem happened: {traceback.format_exc()}")

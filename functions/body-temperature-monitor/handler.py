a6f2558e030b492dda2a5d7590699d4bb17435f2
Gustavo Cassel <guscassel@gmail.com>
2022-09-17 15:50:39 -0300

    Implement simple health service that monitors temperature

--- functions/body-temperature-monitor/handler.py
+++ functions/body-temperature-monitor/handler.py
@@ -1,7 +1,23 @@
+import json
+
+MAX_TEMPERATURE = 38
+MIN_TEMPERATURE = 35
+
+def alert(message):
+    return json.dumps({"send_notification": True, "message": message})
+
+
 def handle(req):
-    """handle a request to the function
+    """Checks if body temperature is out of appropriate range
     Args:
         req (str): request body
     """
 
-    return req
+    vital_sign = json.loads(req)
+
+    if vital_sign["temperature"] > MAX_TEMPERATURE:
+        return alert("Has fever")
+    elif vital_sign["temperature"] < MIN_TEMPERATURE:
+        return alert("Has hypothermia")
+    else:
+        return json.dumps({"send_notification": False})
--- functions/body-temperature-monitor/handler_test.py
+++ functions/body-temperature-monitor/handler_test.py
@@ -1,10 +1,30 @@
-from .handler import handle
+import handler
 
 # Test your handler here
 
 # To disable testing, you can set the build_arg `TEST_ENABLED=false` on the CLI or in your stack.yml
 # https://docs.openfaas.com/reference/yaml/#function-build-args-build-args
 
-def test_handle():
-    # assert handle("input") == "input"
-    pass
+
+def test_fever():
+    input = '{"temperature": 40}'
+    expected = '{"send_notification": true, "message": "Has fever"}'
+    assert handler.handle(input) == expected
+
+
+def test_hypothermia():
+    input = '{"temperature": 34}'
+    expected = '{"send_notification": true, "message": "Has hypothermia"}'
+    assert handler.handle(input) == expected
+
+
+def test_temperature_within_ranges():
+    input = '{"temperature": 37}'
+    expected = '{"send_notification": false}'
+    assert handler.handle(input) == expected
+
+
+if __name__ == "__main__":
+    test_fever()
+    test_hypothermia()
+    test_temperature_within_ranges()

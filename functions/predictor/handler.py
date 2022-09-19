2708f1876b5e143f00c34e80b83931d48713576e
Gustavo Cassel <guscassel@gmail.com>
2022-09-19 20:12:24 -0300

    Create predictor function from template

---
+++ functions/predictor/handler.py
@@ -0,0 +1,6 @@
+def handle(req):
+    """Function template
+    Args:
+        req (str): request body
+    """
+    return req
---
+++ functions/predictor/handler_test.py
@@ -0,0 +1,10 @@
+import handler
+
+# Test your handler here
+
+# To disable testing, you can set the build_arg `TEST_ENABLED=false` on the CLI or in your stack.yml
+# https://docs.openfaas.com/reference/yaml/#function-build-args-build-args
+
+
+if __name__ == "__main__":
+    pass

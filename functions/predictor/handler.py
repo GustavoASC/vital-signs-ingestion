f41ce8d57ebd58a3af2f287597c003f1e249ddca
Gustavo Cassel <guscassel@gmail.com>
2022-09-19 21:33:15 -0300

    Commit first implementation of predictor function

--- functions/predictor/handler.py
+++ functions/predictor/handler.py
@@ -1,6 +1,23 @@
+import json
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
+
+MIN_DATA_POINTS = 2
+
+
+def _round_values(forecast):
+    return [round(elem, 2) for elem in forecast]
+
+
 def handle(req):
-    """Function template
-    Args:
-        req (str): request body
-    """
-    return req
+    input = json.loads(req)
+
+    if not input["data"] or len(input["data"]) < MIN_DATA_POINTS:
+        return json.dumps({"forecast": []})
+
+    fit = Holt(input["data"], initialization_method="estimated").fit(
+        smoothing_level=input["smoothing_level"],
+        smoothing_trend=input["smoothing_trend"],
+        optimized=False,
+    )
+    forecast = fit.forecast(input["future_data_points"]).tolist()
+    return json.dumps({"forecast": _round_values(forecast)})
--- functions/predictor/handler_test.py
+++ functions/predictor/handler_test.py
@@ -1,10 +1,58 @@
 import handler
+import pathlib
 
 # Test your handler here
+CURRENT_DIR = str(pathlib.Path(__file__).parent.resolve())
 
 # To disable testing, you can set the build_arg `TEST_ENABLED=false` on the CLI or in your stack.yml
 # https://docs.openfaas.com/reference/yaml/#function-build-args-build-args
 
 
+def _load_test_resource(name):
+    with open(CURRENT_DIR + "/test-resources/" + name, "r") as file:
+        return file.read()
+
+
+def test_prediction_empty_data():
+    input = _load_test_resource("input-prediction-empty-data.json")
+    expected = '{"forecast": []}'
+    assert handler.handle(input) == expected
+
+
+def test_prediction_single_data_point():
+    input = _load_test_resource("input-prediction-single-data-point.json")
+    expected = '{"forecast": []}'
+    assert handler.handle(input) == expected
+
+
+def test_prediction_two_data_points():
+    input = _load_test_resource("input-prediction-two-data-points.json")
+    expected = '{"forecast": [736.25]}'
+    response = handler.handle(input)
+    assert response == expected
+
+
+def test_prediction_several_data_points_single_future_value():
+    input = _load_test_resource(
+        "input-prediction-several-data-points-single-future-value.json"
+    )
+    expected = '{"forecast": [43.63]}'
+    response = handler.handle(input)
+    assert response == expected
+
+
+def test_prediction_several_data_points_multiple_future_values():
+    input = _load_test_resource(
+        "input-prediction-several-data-points-multiple-future-values.json"
+    )
+    expected = '{"forecast": [43.63, 45.36, 47.1, 48.84, 50.58]}'
+    response = handler.handle(input)
+    assert response == expected
+
+
 if __name__ == "__main__":
-    pass
+    test_prediction_empty_data()
+    test_prediction_single_data_point()
+    test_prediction_two_data_points()
+    test_prediction_several_data_points_multiple_future_values()
+    test_prediction_several_data_points_single_future_value()

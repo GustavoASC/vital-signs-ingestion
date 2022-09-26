8d73b8be788804921da7f7212f66cf17b0112dd6
Gustavo Cassel <guscassel@gmail.com>
2022-09-26 08:16:26 -0300

    Implement first version of Cloud API Adapter

--- functions/modules/cloud-api-adapter/lambda_function.py
+++ functions/modules/cloud-api-adapter/lambda_function.py
@@ -1,8 +1,24 @@
 import json
+import boto3
+
+
+client = boto3.client("lambda")
+health_services = ["body-temperature-monitor", "bar-function"]
+
+
+def invoke_fn(fn_name, payload):
+    client.invoke(
+        FunctionName=fn_name,
+        Payload=payload,
+    )
+
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+
+    if "service_name" in event:
+        invoke_fn(event["service_name"], event["vital_sign"])
+    else:
+        for service in health_services:
+            invoke_fn(service, event["vital_sign"])
+
+    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}

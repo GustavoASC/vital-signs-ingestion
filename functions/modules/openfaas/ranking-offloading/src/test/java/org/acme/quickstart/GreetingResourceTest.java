976d94895d2647ac4c0a601858a664eb2e885c72
EC2 Default User <ec2-user@ip-172-31-6-48.sa-east-1.compute.internal>
2022-09-27 10:57:42 +0000

    Create ranking-offloading function as a quarkus-based function

--- functions/modules/openfaas/ranking-offloading/src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ functions/modules/openfaas/ranking-offloading/src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -1,4 +1,4 @@
-package org.acme;
+package org.acme.quickstart;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
@@ -12,10 +12,10 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/")
           .then()
              .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .body(is("hello"));
     }
 
 }

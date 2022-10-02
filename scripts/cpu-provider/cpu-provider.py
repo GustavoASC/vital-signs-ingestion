3764078fbb55cfc9d031eca72ad2f95505679022
Gustavo Cassel <guscassel@gmail.com>
2022-10-02 18:59:57 -0300

    CPU Provider now returns data as application/json

--- scripts/cpu-provider/cpu-provider.py
+++ scripts/cpu-provider/cpu-provider.py
@@ -14,6 +14,7 @@ class Serv(BaseHTTPRequestHandler):
         }).encode('utf-8')
 
         self.send_response(200)
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(response_bytes)
 

0346f4bff20bdd8018c0250fb5340870dab560a7
Gustavo Cassel <guscassel@gmail.com>
2022-10-03 20:27:14 -0300

    Restore IT test for running services provider

---
+++ functions/modules/openfaas/service-executor/src/test/java/org/acme/quickstart/RunningServicesProviderIT.java
@@ -0,0 +1,34 @@
+package org.acme.quickstart;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.IntStream;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class RunningServicesProviderIT {
+
+    @Inject
+    RunningServicesProviderImpl runningServicesProviderImpl;
+
+    @Test
+    public void shouldStoreAndRetrieveRankingsInMultiThreadScenarios() {
+
+        IntStream.range(1, 100000)
+                .boxed()
+                .parallel()
+                .forEach(i -> {
+                    var id = runningServicesProviderImpl.executionStarted("body-temperature-monitor", 7);
+                    runningServicesProviderImpl.executionFinished(id);
+                });
+
+        assertThat(runningServicesProviderImpl.getRankingsForRunningServices())
+                .isEmpty();
+    }
+
+}

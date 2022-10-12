9e0a61f73508a298915b6962acbc8b37354a135a
Gustavo Cassel <guscassel@gmail.com>
2022-10-12 14:29:28 -0300

    Update Cloud API Adapter

--- functions/modules/aws/cloud-api-adapter/lambda_function.py
+++ functions/modules/aws/cloud-api-adapter/lambda_function.py
@@ -7,6 +7,7 @@ health_services = ["body-temperature-monitor"]
 
 
 def invoke_fn(fn_name, payload):
+    print("Invoking function " + fn_name + "...")
     client.invoke(
         FunctionName=fn_name,
         Payload=payload,
@@ -14,11 +15,15 @@ def invoke_fn(fn_name, payload):
 
 
 def lambda_handler(event, context):
+    print(event)
 
-    if "service_name" in event:
-        invoke_fn(event["service_name"], event["vital_sign"])
+    body = event["body"]
+    payload = json.loads(body)
+
+    if "service_name" in payload:
+        invoke_fn(payload["service_name"], payload["vital_sign"])
     else:
         for service in health_services:
-            invoke_fn(service, event["vital_sign"])
+            invoke_fn(service, payload["vital_sign"])
 
     return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}

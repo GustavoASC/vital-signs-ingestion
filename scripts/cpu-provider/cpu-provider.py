d28343c05cd1e5b2271cb7b24fc2421782c1ae16
Gustavo Cassel <guscassel@gmail.com>
2022-10-12 09:44:24 -0300

    Update CPU provider endpoint to return a previously calculated CPU percentage

--- scripts/cpu-provider/cpu-provider.py
+++ scripts/cpu-provider/cpu-provider.py
@@ -1,22 +1,38 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from threading import Thread
 import psutil
 import json
+import time
+
+cpu_percent = 0.0
+
 
 class Serv(BaseHTTPRequestHandler):
-
     def do_GET(self):
-
-        cpu_percent = psutil.cpu_percent()
-        print(f"Current cpu: {cpu_percent}")
-
-        response_bytes = json.dumps({
-            'cpu': cpu_percent
-        }).encode('utf-8')
-
+        global cpu_percent
+        response_bytes = json.dumps({"cpu": cpu_percent}).encode("utf-8")
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header("Content-type", "application/json")
         self.end_headers()
         self.wfile.write(response_bytes)
 
-httpd = HTTPServer(('', 8099), Serv)
-httpd.serve_forever()
+
+def update_cpu_percent_loop():
+    global cpu_percent
+    while True:
+        cpu_percent = psutil.cpu_percent(1)
+
+
+if __name__ == "__main__":
+
+    # Starts a thread to periodically update CPU percentage
+    thread = Thread(target=update_cpu_percent_loop)
+    thread.start()
+
+    # Waits a small amount of time to ensure that CPU percentage
+    # has been updated at least once
+    time.sleep(1.5)
+
+    # Starts the HTTP server to listen for GET CPU requests
+    httpd = HTTPServer(("", 8099), Serv)
+    httpd.serve_forever()

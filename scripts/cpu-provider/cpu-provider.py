e8e0a24ce68291df1286377c2126740cc7926ef8
Gustavo Cassel <guscassel@gmail.com>
2022-11-15 11:16:14 -0300

    Use scheduler lib to schedule CPU collection tasks

--- scripts/cpu-provider/cpu-provider.py
+++ scripts/cpu-provider/cpu-provider.py
@@ -3,15 +3,18 @@ from threading import Thread
 import psutil
 import json
 import time
+import schedule
 
-update_interval = 1
+update_interval = 5
+total_threads = 5
+
 cpu_percent = 0.0
 
-class Serv(BaseHTTPRequestHandler):
 
+class Serv(BaseHTTPRequestHandler):
     def do_GET(self):
         global cpu_percent
-        
+
         print("Current amount of cpu: " + str(cpu_percent))
         print("Current amount of memory: " + str(psutil.virtual_memory()))
 
@@ -21,15 +24,18 @@ class Serv(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(response_bytes)
 
-
     def do_POST(self):
         global update_interval
+        global total_threads
 
-        content_len = int(self.headers.get('Content-Length'))
+        content_len = int(self.headers.get("Content-Length"))
         post_body = self.rfile.read(content_len)
-
         settings = json.loads(post_body)
-        update_interval = settings['update_interval']
+
+        _finalize_all_threads()
+        update_interval = settings["update_interval"]
+        total_threads = settings["total_threads"]
+        _initialize_threads()
 
         self.send_response(200)
         self.send_header("Content-type", "application/json")
@@ -37,22 +43,43 @@ class Serv(BaseHTTPRequestHandler):
         self.wfile.write(json.dumps({}).encode("utf-8"))
 
 
+def job_thread():
+    thread = Thread(target=update_cpu_percent_loop)
+    thread.start()
+
+
 def update_cpu_percent_loop():
     global cpu_percent
-    while True:
-        cpu_percent = psutil.cpu_percent(update_interval)
+    global update_interval
+    cpu_percent = psutil.cpu_percent(update_interval)
+
+
+def _initialize_threads():
+    global update_interval
+    global total_threads
+
+    thread_start_offset = update_interval / total_threads
+
+    print("Initiating schedule...")
+    for i in range(total_threads):
+        schedule.every(update_interval + 0.2).seconds.do(job_thread)
+        time.sleep(thread_start_offset)
+
+
+def _finalize_all_threads():
+    print("Clearing all schedules...")
+    schedule.clear()
 
 
 if __name__ == "__main__":
 
-    # Starts a thread to periodically update CPU percentage
-    thread = Thread(target=update_cpu_percent_loop)
-    thread.start()
-
-    # Waits a small amount of time to ensure that CPU percentage
-    # has been updated at least once
-    time.sleep(1.5)
+    _initialize_threads()
 
     # Starts the HTTP server to listen for GET CPU requests
+    print("Starting the HTTP server...")
     httpd = HTTPServer(("", 8099), Serv)
-    httpd.serve_forever()
+    Thread(target=httpd.serve_forever).start()
+
+while True:
+    schedule.run_pending()
+    time.sleep(0.01)

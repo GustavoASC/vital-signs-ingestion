df21209e987599016a258dbebbcbe7b1e1fabf98
Gustavo Cassel <guscassel@gmail.com>
2022-11-24 21:31:06 -0300

    Improve lambda function to send notification after the health service is executed on the cloud

--- functions/modules/aws/cloud-api-adapter/lambda_function.py
+++ functions/modules/aws/cloud-api-adapter/lambda_function.py
@@ -1,6 +1,9 @@
 import json
 import boto3
+import datetime
+from urllib import request, error
 
+RESULTS_URL = "https://webhook.site/42189449-4fef-42ca-b1f6-23ff5493c405"
 
 client = boto3.client("lambda")
 health_services = ["body-temperature-monitor"]
@@ -12,6 +15,21 @@ def invoke_fn(fn_name, payload):
         FunctionName=fn_name,
         Payload=payload,
     )
+
+
+def notify_vital_sign_processed(id):
+    try:
+        end_timestamp = int(datetime.datetime.now().timestamp() * 1000)
+        data = {"end_timestamp": end_timestamp}
+        req = request.Request(
+            url="{}/{}".format(RESULTS_URL, id),
+            data=json.dumps(data).encode("utf-8"),
+            method="PATCH",
+        )
+        request.urlopen(req)
+    except error.HTTPError as e:
+        body = e.read().decode()
+        print(body)
 
 
 def lambda_handler(event, context):
@@ -26,4 +44,6 @@ def lambda_handler(event, context):
         for service in health_services:
             invoke_fn(service, payload["vital_sign"])
 
-    return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
+    notify_vital_sign_processed(payload["id"])
+
+    return {"statusCode": 200, "body": json.dumps("")}

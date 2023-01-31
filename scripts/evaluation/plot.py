988ab3621d595399833aa0e24f7b89b77a028246
Gustavo Cassel <guscassel@gmail.com>
2023-01-31 19:33:54 -0300

    Include additional plots and summary information

--- scripts/evaluation/plot.py
+++ scripts/evaluation/plot.py
@@ -3,17 +3,20 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 import datetime as dt
+import datetime
+from datetime import timedelta
+import matplotlib.dates as mdates
 
 
 http = urllib3.PoolManager()
 save_chart_as_file = True
-include_title = False
+include_title = True
 
 global_result_dir = ""
 
 
 def _initialize_chart():
-    plt.figure(figsize=(20, 7.5))
+    plt.figure(figsize=(10, 5))
     _increase_font_size()
 
 
@@ -41,8 +44,11 @@ def plot_all_charts(result_dir, cpu_usage, mem_usage, all_data):
     global_result_dir = result_dir
 
     _plot_chart_response_time(all_data)
+    _plot_vital_signs_generated_per_minute(all_data)
     _plot_throughput(all_data)
     _plot_response_time(all_data)
+    _plot_smaller_response_times(all_data)
+    _plot_user_priority()
 
     for machine_name, current_cpu_node_data in cpu_usage.items():
         _plot_chart_cpu_usage(machine_name, current_cpu_node_data)
@@ -85,9 +91,15 @@ def _plot_chart_cpu_usage(machine_name, cpu_usage):
         color="moccasin",
         linestyle="--",
     )
-    _set_title("CPU Usage during tests for machine {}".format(machine_name))
-    plt.xlabel("Timestamp")
-    plt.ylabel("CPU Usage")
+
+    letter_fog_node = machine_name[-1]
+    _set_title("CPU usage on fog node {}".format(letter_fog_node))
+    plt.xlabel("Moment when CPU usage was collected")
+    plt.ylabel("CPU usage")
+
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+    plt.setp(plt.gca().get_xticklabels(), rotation=60, ha="right")
+
     _finalize_chart("cpu_usage_{}.png".format(machine_name))
 
 
@@ -115,16 +127,49 @@ def _plot_chart_mem_usage(machine_name, mem_usage):
 def _plot_chart_response_time(all_data):
     _initialize_chart()
     legend = []
+    
     for key, thread_data in sorted(all_data.items()):
-        legend.append(key)
-        plt.plot(thread_data["start_datetime"], thread_data["elapsed"])
 
-    _set_title("Response time for all threads")
-    plt.xlabel("Timestamp")
-    plt.ylabel("Response time")
+        # Converts UTC-3 to UTC
+        timezone_correct = []
+        for current_dt in thread_data["start_datetime"]:
+            timezone_correct.append(current_dt + timedelta(hours=3))
+
+
+        legend.append(_translate_user_priority_label(key))
+        plt.plot(timezone_correct, thread_data["elapsed"])
+
+    _set_title("Response time for each user priority")
+    plt.xlabel("Execution moment")
+    plt.ylabel("Response time (millis)")
     plt.legend(legend)
+    plt.setp(plt.gca().get_xticklabels(), rotation=60, ha="right")
+
     _finalize_chart("response_time.png")
 
+
+def _plot_vital_signs_generated_per_minute(all_data):
+    _initialize_chart()
+    import collections
+    data = []
+    for key, thread_data in sorted(all_data.items()):
+        start_datetime = thread_data["start_datetime"]
+
+        for current_dt in start_datetime:
+            data.append(current_dt.replace(microsecond=0, second=0))
+            pass
+
+    
+    counter = collections.Counter(data)
+    keys = counter.keys()
+    values = counter.values()
+
+    keys, values = zip(*sorted(zip(keys, values)))
+
+    plt.plot(keys, values, color="b")
+    plt.xlabel("Timestamp")
+    plt.ylabel("Vital signs")
+    _finalize_chart("vital_signs_generated.png")
 
 def _plot_offloading_histogram(all_data, fog_name):
     _initialize_chart()
@@ -197,7 +242,7 @@ def _plot_stacked_local_executions_with_offloading(all_data, fog_name):
     for key, thread_data in sorted(all_data.items()):
         fog_data = thread_data["fog_nodes_data"][fog_name]
 
-        x.append(key)
+        x.append(_translate_user_priority_label(key))
         local_execution.append(fog_data["total_local_execution"])
         exceeded_critical_cpu.append(fog_data["total_exceeded_critical_cpu_threshold"])
         heuristic_by_ranking.append(fog_data["total_result_for_heuristic_by_ranking"])
@@ -249,12 +294,14 @@ def _plot_stacked_local_executions_with_offloading(all_data, fog_name):
     #     label="Local exec by fallback",
     # )
 
+    letter_fog_node = fog_name[-1]
     _set_title(
-        f"Execution operations according to user priority on fog node {fog_name}"
+        f"Ingestion operations on fog node " + letter_fog_node
     )
     plt.xlabel("User priority")
-    plt.ylabel("Execution operations")
-    plt.legend(loc="lower right")
+    plt.ylabel("Ingestion operations")
+    plt.legend(loc="lower right", fancybox=True, framealpha=1)
+    plt.setp(plt.gca().get_xticklabels(), rotation=60, ha="right")
     _finalize_chart("execution_operations_with_offloading_{}.png".format(fog_name))
 
 
@@ -263,13 +310,16 @@ def _plot_throughput(all_data):
     x = []
     y = []
     for key, thread_data in sorted(all_data.items()):
-        x.append(key)
+        x.append(_translate_user_priority_label(key))
         y.append(thread_data["throughput_seconds"])
 
     plt.bar(x, y, color="g", width=0.4)
-    _set_title("Throughput (seconds) according to user priority")
+    _set_title("Throughput according to user priority")
     plt.xlabel("User priority")
-    plt.ylabel("Throughput (seconds)")
+    plt.ylabel("Throughput (vital signs/sec)")
+
+    plt.setp(plt.gca().get_xticklabels(), rotation=60, ha="right")
+
     _finalize_chart("throughput.png")
 
 
@@ -288,7 +338,7 @@ def _plot_response_time(all_data):
     avg_response_time = []
     min_response_time = []
     for key, thread_data in sorted(all_data.items()):
-        x.append(key)
+        x.append(_translate_user_priority_label(key))
         max_response_time.append(thread_data["maximum"] / 1000)
         p99_response_time.append(thread_data["percentile_99"] / 1000)
         p95_response_time.append(thread_data["percentile_95"] / 1000)
@@ -349,7 +399,7 @@ def _plot_response_time(all_data):
     )
 
     plt.xticks(x_axis, x)
-    _set_title("Response time (seconds) according to user priority")
+    _set_title("Response time (seconds) for higher percentiles according to user priority")
     plt.xlabel("User priority")
     plt.ylabel("Response time (seconds)")
     plt.legend(
@@ -357,9 +407,80 @@ def _plot_response_time(all_data):
     )
 
     current_values = plt.gca().get_yticks()
-    plt.gca().set_yticklabels(["{:,.0f}".format(x) for x in current_values])
+    plt.gca().set_yticklabels(["{:,.0f}".format(x).replace(",", ".") for x in current_values])
+    # plt.setp(plt.gca().get_xticklabels(), rotation=60, ha="right")
 
     _finalize_chart("response_time_with_priority.png")
+
+
+
+def _plot_smaller_response_times(all_data):
+    _initialize_chart()
+
+    x = []
+    p50_response_time = []
+    p40_response_time = []
+    p30_response_time = []
+    p20_response_time = []
+    p10_response_time = []
+    for key, thread_data in sorted(all_data.items()):
+        x.append(_translate_user_priority_label(key))
+        p50_response_time.append(thread_data["percentile_50"] / 1000)
+        p40_response_time.append(thread_data["percentile_40"] / 1000)
+        p30_response_time.append(thread_data["percentile_30"] / 1000)
+        p20_response_time.append(thread_data["percentile_20"] / 1000)
+        p10_response_time.append(thread_data["percentile_10"] / 1000)
+
+    x_axis = np.arange(len(x))
+    plt.bar(
+        x_axis - 0.2, p50_response_time, color="b", width=0.1, label="50th percentile"
+    )
+    plt.bar(
+        x_axis - 0.1, p40_response_time, color="g", width=0.1, label="40th percentile"
+    )
+    plt.bar(
+        x_axis,       p30_response_time, color="r", width=0.1, label="30th percentile"
+    )
+    plt.bar(
+        x_axis + 0.1, p20_response_time, color="y", width=0.1, label="20th percentile"
+    )
+    plt.bar(
+        x_axis + 0.2, p10_response_time, color="m", width=0.1, label="10th percentile"
+    )
+
+    plt.xticks(x_axis, x)
+    _set_title("Response time (seconds) for lower percentiles according to user priority")
+    plt.xlabel("User priority")
+    plt.ylabel("Response time (seconds)")
+    plt.legend(
+        loc="center left", bbox_to_anchor=(0.85, 0.5), fancybox=True, framealpha=1
+    )
+
+    current_values = plt.gca().get_yticks()
+    plt.gca().set_yticklabels(["{:,.0f}".format(x).replace(",", ".") for x in current_values])
+
+    _finalize_chart("smaller_response_time_with_priority.png")
+
+def _plot_user_priority():
+    _initialize_chart()
+    x = [_translate_user_priority_label("1"), _translate_user_priority_label("2"), _translate_user_priority_label("3"), _translate_user_priority_label("4"), _translate_user_priority_label("5")]
+    ranking_body_temperature_monitor = [3, 5, 7, 9, 11]
+    ranking_heart_failure_predictor = [7, 9, 11, 13, 15]
+
+    x_axis = np.arange(len(x))
+    plt.xticks(x_axis, x)
+
+    plt.bar(x_axis - 0.2, ranking_body_temperature_monitor, color="g", width=0.4, label="Body temperature monitor")
+    plt.bar(x_axis + 0.2, ranking_heart_failure_predictor, color="b", width=0.4, label="Heart failure predictor")
+    _set_title("Rankings for service and user priority")
+    plt.xlabel("User priority")
+    plt.ylabel("Calculated ranking")
+    plt.legend(loc="upper left")
+
+    plt.setp(plt.gca().get_xticklabels(), rotation=60, ha="right")
+
+    _finalize_chart("rankings.png")
+
 
 
 def _plot_offloading_reasons(all_data, fog_name):
@@ -420,11 +541,32 @@ def _read_json(path):
         return eval(content)
 
 
+def _update_percentiles(all_data):
+    for user_priority, thread_data in all_data.items():
+        thread_data["percentile_40"] = np.percentile(thread_data["elapsed"], 40)
+        thread_data["percentile_30"] = np.percentile(thread_data["elapsed"], 30)
+        thread_data["percentile_20"] = np.percentile(thread_data["elapsed"], 20)
+        thread_data["percentile_10"] = np.percentile(thread_data["elapsed"], 10)
+        
+def _translate_user_priority_label(user_priority):
+    if user_priority == "1":
+        return "Very healthy"
+    elif user_priority == "2":
+        return "Healthy"
+    elif user_priority == "3":
+        return "Warning"
+    elif user_priority == "4":
+        return "Critical"
+    elif user_priority == "5":
+        return "Very critical"
+
+
 if __name__ == "__main__":
 
     result_dir = "<include results dir here>"
 
     all_data = _read_json(f"{result_dir}/analyzed-dataset.json")
+    _update_percentiles(all_data)
 
     cpu_usage = {}
     mem_usage = {}
--- scripts/evaluation/summary.py
+++ scripts/evaluation/summary.py
@@ -1,5 +1,6 @@
 import logging
 import datetime
+import numpy as np
 
 
 def print_summary(all_data):
@@ -21,6 +22,10 @@ def _print_summary_current_key(key, thread_data):
     logging.info("##     70th percentile: {}".format(thread_data["percentile_70"] / 1000))
     logging.info("##     60th percentile: {}".format(thread_data["percentile_60"] / 1000))
     logging.info("##     50th percentile: {}".format(thread_data["percentile_50"] / 1000))
+    logging.info("##     40th percentile: {}".format(thread_data["percentile_40"] / 1000))
+    logging.info("##     30th percentile: {}".format(thread_data["percentile_30"] / 1000))
+    logging.info("##     20th percentile: {}".format(thread_data["percentile_20"] / 1000))
+    logging.info("##     10th percentile: {}".format(thread_data["percentile_10"] / 1000))
     logging.info("##             Average: {}".format(thread_data["average"] / 1000))
 
 
@@ -33,11 +38,16 @@ def _print_summary_current_key(key, thread_data):
     percentile_70 = round(thread_data["percentile_70"] / 1000, 3)
     percentile_60 = round(thread_data["percentile_60"] / 1000, 3)
     percentile_50 = round(thread_data["percentile_50"] / 1000, 3)
+    percentile_40 = round(thread_data["percentile_40"] / 1000, 3)
+    percentile_30 = round(thread_data["percentile_30"] / 1000, 3)
+    percentile_20 = round(thread_data["percentile_20"] / 1000, 3)
+    percentile_10 = round(thread_data["percentile_10"] / 1000, 3)
     average = round(thread_data["average"] / 1000, 3)
 
     std_dev = "?"
 
-    print(f"{key} & {maximum} & {percentile_99} & {percentile_95} & {percentile_90} & {percentile_80} & {percentile_70} & {percentile_60} & {percentile_50} & {average} & {minimum} & {std_dev} \\\\")
+    # print(f"{key} & {percentile_99} & {percentile_95} & {percentile_90} & {percentile_80} & {percentile_70} & {percentile_60} & {percentile_50} & {percentile_40} & {percentile_30} & {percentile_20} & {percentile_10} \\\\")
+    print(f"{key} & {maximum} & {minimum} & {average} & {std_dev} \\\\")
 
     for fog_node_name in thread_data["fog_nodes_data"]:
         logging.info("## Current fog node name: {}".format(fog_node_name))
@@ -88,6 +98,35 @@ def _print_fog_node_data(fog_node_data):
         )
     )
 
+def _print_execution_operations_with_offloading(all_data):
+
+    print("Fog name & Priority & Local execution & Exceeded critical CPU & Ranking heuristic & Duration heuristic & Fallback \\\\")
+    for priority, thread_data in sorted(all_data.items()):
+        for fog_name in thread_data["fog_nodes_data"]:
+            fog_data = thread_data["fog_nodes_data"][fog_name]
+
+            local_execution = fog_data["total_local_execution"]
+            exceeded_critical_cpu = fog_data["total_exceeded_critical_cpu_threshold"]
+            heuristic_by_ranking = fog_data["total_result_for_heuristic_by_ranking"]
+            heuristic_by_duration = fog_data["total_result_for_heuristic_by_duration"]
+            heuristic_by_fallback = fog_data["total_assuming_fallback_for_heuristics"]
+
+            print(f"{fog_name} & {priority} & {local_execution} & {exceeded_critical_cpu} & {heuristic_by_ranking} & {heuristic_by_duration} & {heuristic_by_fallback} \\\\")
+            
+
+
+
+            pass
+
+    pass
+
+def _update_percentiles(all_data):
+    for user_priority, thread_data in all_data.items():
+        thread_data["percentile_40"] = np.percentile(thread_data["elapsed"], 40)
+        thread_data["percentile_30"] = np.percentile(thread_data["elapsed"], 30)
+        thread_data["percentile_20"] = np.percentile(thread_data["elapsed"], 20)
+        thread_data["percentile_10"] = np.percentile(thread_data["elapsed"], 10)
+
 
 def _read_json(path):
     with open(path) as f:
@@ -106,4 +145,6 @@ if __name__ == "__main__":
     result_dir = "<results dir>"
 
     all_data = _read_json(f"{result_dir}/analyzed-dataset.json")
+    _update_percentiles(all_data)
     print_summary(all_data)
+    _print_execution_operations_with_offloading(all_data)
